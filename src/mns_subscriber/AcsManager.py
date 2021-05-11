01b6bc8dd636769bfdd77f5d2e88565d79305a02
1095222570 <1095222570@qq.com>
2021-05-11 17:01:02 +0800

    订单日志

--- src/mns_subscriber/AcsManager.py
+++ src/mns_subscriber/AcsManager.py
@@ -223,7 +223,8 @@ class AcsManager(object):
 
         now = datetime.now()
         cur_hour = now.hour
-        print "------------cnt----------------{}".format(cnt)
+        from mns_subscriber import config
+        config.logger.error("------------cnt----------------{}".format(cnt))
         odd_even = cnt % 2
         # 是上车就入集合
         k = RedisKey.STUDENT_SET.format(dev_name)
