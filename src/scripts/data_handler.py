2e13d5b8e21f6849aeb989ec5f1c1146eca84859
1095222570 <1095222570@qq.com>
2021-07-22 14:48:57 +0800

    scripts PRO

--- src/scripts/data_handler.py
+++ src/scripts/data_handler.py
@@ -10,7 +10,7 @@ from aliyunsdkcore.client import AcsClient
 from aliyunsdkiot.request.v20180120.RegisterDeviceRequest import \
     RegisterDeviceRequest
 from aliyunsdkiot.request.v20180120.PubRequest import PubRequest
-import db
+from scripts import db
 import config
 
 
@@ -65,6 +65,7 @@ class DataHandler(object):
 
             # feature
             for mfr_id in mfr_list:
+                # 厂商是武汉就添加feature数据
                 # 是否存在
                 feature_row = pgsql_db.get(
                     pgsql_cur, query_feature_sql.format(face_id, mfr_id))
@@ -76,14 +77,19 @@ class DataHandler(object):
                     if face_status == 1:
                         d['status'] = -1
                     else:
+                        # 等待处理
                         if face_status == 2:
                             d['status'] = 1
+                        # 处理中
                         if face_status == 3:
                             d['status'] = 2
+                        # 有效
                         if face_status == 4:
                             d['status'] = 3
+                        # 预期数据准备失败
                         if face_status == 5:
                             d['status'] = 4
+                        # 过期
                         if face_status == 6:
                             d['status'] = 3
                         d['oss_url'] = oss_url if oss_url else 'null'
